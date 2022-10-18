aa585fcd99347e410a9bd55b9f71c36ab9b4f8c1
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-10-18 09:53:02 -0500

    PokemonsResponse

---
+++ lib/models/pokemons_response_model.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+class PokemonsResponse {
+  PokemonsResponse({
+    required this.count,
+    required this.next,
+    this.previous,
+    required this.results,
+  });
+
+  int count;
+  String next;
+  String? previous;
+  List<Result> results;
+
+  PokemonsResponse copyWith({
+    required int count,
+    required String next,
+    String? previous,
+    required List<Result> results,
+  }) => PokemonsResponse(
+    count: count,
+    next: next,
+    previous: previous,
+    results: results,
+  );
+
+  factory PokemonsResponse.fromJson(String str) => PokemonsResponse.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory PokemonsResponse.fromMap(Map<String, dynamic> json) => PokemonsResponse(
+    count:    json["count"],
+    next:     json["next"],
+    previous: json["previous"],
+    results:  List<Result>.from(json["results"].map((x) => Result.fromMap(x))),
+  );
+
+  Map<String, dynamic> toMap() => {
+    "count":    count,
+    "next":     next,
+    "previous": previous,
+    "results":  List<dynamic>.from(results.map((x) => x.toMap())),
+  };
+}
+
+class Result {
+  Result({
+    required this.name,
+    required this.url,
+  });
+
+  String name;
+  String url;
+
+  Result copyWith({
+    required String name,
+    required String url,
+  }) => Result(
+    name: name,
+    url: url,
+  );
+
+  factory Result.fromJson(String str) => Result.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Result.fromMap(Map<String, dynamic> json) => Result(
+    name: json["name"],
+    url:  json["url"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "name": name,
+    "url":  url,
+  };
+}
