076d5b2fb984647442f693f0c56d29e5045deb97
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-12-17 19:30:47 -0600

    HomeScreen design finish

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: unused_local_variable
+
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
@@ -12,7 +15,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late List pokedex;
+  List? pokedex;
 
   @override
   void initState() {
@@ -28,9 +31,9 @@ class _HomeScreenState extends State<HomeScreen> {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
 
-    Color _greenColor = Color(0xff2a9d8f);
-    Color _redColor = Color(0xffe76f51);
-    Color _blueColor = Color(0xff37A5C6);
+    Color _greenColor = const Color(0xff2a9d8f);
+    Color _redColor = const Color(0xffe76f51);
+    Color _blueColor = const Color(0xff37A5C6);
 
     return SafeArea(
       child: Scaffold(
@@ -56,6 +59,136 @@ class _HomeScreenState extends State<HomeScreen> {
                   fontWeight: FontWeight.bold,
                   fontSize: 30
                 ),
+              ),
+            ),
+
+            Positioned(
+              top: 150,
+              bottom: 0,
+              width: width,
+              child: Column(
+                children: [
+                  pokedex != null 
+                    ? Expanded(
+                      child: GridView.builder(
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 1.4
+                        ), 
+                        shrinkWrap: true,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: pokedex!.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: InkWell(
+                              child: SafeArea(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: pokedex![index]['type'][0] == "Grass" ? Colors.greenAccent : pokedex![index]['type'][0] == "Fire" ? Colors.redAccent 
+                                      : pokedex![index]['type'][0] == "Water" ? Colors.blue : pokedex![index]['type'][0] == "Poison" ? Colors.deepPurpleAccent 
+                                      : pokedex![index]['type'][0] == "Electric" ? Colors.amber : pokedex![index]['type'][0] == "Rock" ? Colors.grey 
+                                      : pokedex![index]['type'][0] == "Ground" ? Colors.brown : pokedex![index]['type'][0] == "Psychic" ? Colors.indigo 
+                                      : pokedex![index]['type'][0] == "Fighting" ? Colors.orange : pokedex![index]['type'][0] == "Bug" ? Colors.lightGreenAccent 
+                                      : pokedex![index]['type'][0] == "Ghost" ? Colors.deepPurple : pokedex![index]['type'][0] == "Normal" ? Colors.black26 : Colors.pink,
+                                    borderRadius: const BorderRadius.all(Radius.circular(25))
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                        top: -10,
+                                        right: -10,
+                                        child: Image.asset(
+                                          'assets/images/pokeball.png',
+                                          height: 100,
+                                          fit: BoxFit.fitHeight,
+                                        )
+                                      ),
+                                      Positioned(
+                                        bottom: 5,
+                                        right: 5,
+                                        child: Hero(
+                                          tag: index,
+                                          child: CachedNetworkImage(
+                                            imageUrl: pokedex![index]['img'],
+                                            height: 100,
+                                            fit: BoxFit.fitHeight,
+                                            placeholder: (context, url) => const Center(
+                                              child: CircularProgressIndicator(),
+                                            ),
+                                          ),
+                                        )
+                                      ),
+                                      Positioned(
+                                        top: 55,
+                                        left: 15,
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: const BorderRadius.all(Radius.circular(20)),
+                                            color: Colors.black.withOpacity(0.5)
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
+                                            child: Text(
+                                              pokedex![index]['type'][0],
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                shadows: [
+                                                  BoxShadow(
+                                                    color: Colors.blueGrey,
+                                                    offset: Offset(0, 0),
+                                                    spreadRadius: 1.0,
+                                                    blurRadius: 15
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ),
+                                      Positioned(
+                                        top: 30,
+                                        left: 15,
+                                        child: Text(
+                                          pokedex![index]['name'],
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                            color: Colors.white,
+                                            shadows: [
+                                              BoxShadow(
+                                                color: Colors.blueGrey,
+                                                offset: Offset(0, 0),
+                                                spreadRadius: 1.0,
+                                                blurRadius: 15
+                                              )
+                                            ]
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              onTap: () {
+                                //TODO: AHHHHHHH
+                              },
+                            ),
+                          );
+                        }
+                      ),
+                    )
+                    : const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 0,
+              child: SizedBox(
+                height: 150,
+                width: width,
               ),
             ),
           ],
